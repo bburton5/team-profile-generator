58e40a7efc9f0adf9e5f02168cb69b0f9e0d1a54
bburton5 <bburton5@uw.edu>
2022-09-13 21:49:41 -0700

    added all appropriate classes and their prospective properties and methods

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-node_modules;
+// Includes packages needed for this application
+const inquirer = require("inquirer");
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
